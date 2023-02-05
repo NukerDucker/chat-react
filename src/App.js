5c118ba30c19c81814e022f8226f36f14aae7a4c
NukerDucker <np.intharasing@gmai.com>
2023-02-05 15:51:58 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, Fragment } from 'react';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/database';
+import 'firebase/compat/auth';
+import { Button, Form, List, Container, Header } from 'semantic-ui-react';
 
-function App() {
+const Chat = () => {
+  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState('');
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const firebaseConfig = {
+      apiKey: "AIzaSyDD1VXc8X2onaRsPky1a_eJdPbl2R2_77o",
+      authDomain: "cholechatweb-f3f7c.firebaseapp.com",
+      databaseURL: "https://cholechatweb-f3f7c-default-rtdb.asia-southeast1.firebasedatabase.app",
+      projectId: "cholechatweb-f3f7c",
+      storageBucket: "cholechatweb-f3f7c.appspot.com",
+      messagingSenderId: "1042820584496",
+      appId: "1:1042820584496:web:25a7988ab8242cd4f889f5",
+    };
+    firebase.initializeApp(firebaseConfig);
+
+    const database = firebase.database();
+    const messagesRef = database.ref('messages');
+    
+    messagesRef.on('value', (snapshot) => {
+      setMessages(Object.values(snapshot.val()));
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      setUser(user);
+    });
+  }, []);
+
+  const submitMessage = (e) => {
+    e.preventDefault();
+    if (!user) {
+      return alert('You must sign in first');
+    }
+    if (!input) {
+      return alert('You must enter a message');
+    }
+    const database = firebase.database();
+    const messagesRef = database.ref('messages');
+
+    messagesRef.push({
+      content: input,
+      timestamp: firebase.database.ServerValue.TIMESTAMP,
+      sender: user.displayName,
+    });
+
+    setInput('');
+  };
+
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+      .then(result => {
+        const user = result.user;
+        setUser(user);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  const signOut = () => {
+    firebase.auth().signOut().then(() => {
+      setUser(null);
+    }).catch(error => {
+      console.error(error);
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container text style={{marginTop: '10px'}}>
+      <Header as='h2'>Chat</Header>
+      {!user && (
+        <Button onClick={signInWithGoogle} size="big" primary style={{marginBottom: '10px'}}>
+          Sign in with Google
+        </Button>
+      )}
+      {user && (
+        <Fragment>
+          <Header as='h3'>Welcome, {user.email}</Header>
+          <Button onClick={signOut} size="big" style={{marginBottom: '10px'}}>
+            Sign out
+          </Button>
+        </Fragment>
+      )}
+      <Form onSubmit={submitMessage} style={{marginTop: '10px'}}>
+        <Form.Input
+          placeholder="Enter a message"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          style={{marginBottom: '10px'}}
+        />
+        <Button type="submit">Submit</Button>
+      </Form>
+      <List style={{marginTop: '10px'}}>
+        {messages.map((message, i) => (
+          <List.Item key={i}>
+            <List.Header>{message.sender}</List.Header>
+            <List.Description>{message.content}</List.Description>
+          </List.Item>
+        ))}
+      </List>
+    </Container>
   );
-}
 
-export default App;
+  };
+  
+  export default Chat;
+  
